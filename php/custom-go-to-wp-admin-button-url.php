7aeb13bd02e09d4f019af3ed1b0094a1b8e6676a
Keith Crain <kwcjr@users.noreply.github.com>
2023-08-23 11:35:00 -0400

    Update custom-go-to-wp-admin-button-url.php
    tested compatibility for wp v6.3 & MainWP v4.5

--- php/custom-go-to-wp-admin-button-url.php
+++ php/custom-go-to-wp-admin-button-url.php
@@ -1,4 +1,17 @@
-<?php
+<?php // DO NOT INCLUDE TAG WHEN PASTING SNIPPET
+
+// Copy here down... 
+
+/**
+ * @snippet       Custom "Go-to-Admin" Button URL
+ * @author        MainWP
+ * @author_url    https://MainWP.com
+ * @testedwith    WordPress v6.3
+ * @testedwith    MainWP Dashboard v4.5
+ *
+ * @param         admin_url( 'plugins.php' );
+ * @How-to-use    Change the page slug with admin_url('PAGE_SLUG.php').
+ */
 
 //Set custom url for the Go to WP Admin button. In this example, we are setting the button to go to the WP > Plugins > Installed Plugins page
 

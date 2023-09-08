031d3db381d435728ecc48dec5970245246e2035
Keith Crain <kwcjr@users.noreply.github.com>
2023-09-08 19:33:53 -0400

    Update custom-mainwp-menu-header.php
    Tested compatibility with WP v6.3 & MainWP v4.5.1.1

--- php/custom-mainwp-menu-header.php
+++ php/custom-mainwp-menu-header.php
@@ -1,11 +1,23 @@
-<?php 
+<?php // DO NOT INCLUDE TAG WHEN PASTING SNIPPET
+
+// Copy here down... 
+
+/**
+ * @snippet       Change the MainWP WP Admin menu label.
+ * @author        MainWP
+ * @author_url    https://mainwp.com
+ * @testedwith    WordPress v6.3
+ * @testedwith    MainWP Dashboard v4.5.1.1
+ * 
+ * @param         $item[0] = 'Demo Label'; ( change this to what you want ) 
+ */
 
 add_action( 'admin_head', 'mycustom_menu_admin_head' );
 function mycustom_menu_admin_head() {
     global $menu;
     foreach( $menu as &$item ) {
         if ( $item[2] == 'mainwp_tab' ) {
-             $item[0] = 'Demo Header';
+             $item[0] = 'Demo Label';
           break;
         }
     }
